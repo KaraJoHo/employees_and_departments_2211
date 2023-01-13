03641930dc559ff420d7ceb5abb1920efeb833c9
KaraJH <karas.jones@yahoo.com>
2023-01-13 09:48:40 -0700

    test: Departments and add department methods

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -6,15 +6,27 @@ RSpec.describe Budget do
   let(:budget) {Budget.new(2010)}
 
   let(:customer_service) {Department.new("Customer Service")}
+  let(:tech_support) {Department.new("Tech Support")}
 
-  let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
-  let(:aaron) {Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})}
+  let(:jermajesty) {Employee.new({name: "Jermajesty Jackson", age: "30", salary: "$100000"})}
+  let(:chip) {Employee.new({name: "Chip Munk", age: "25", salary: "90000"})}
+  let(:inigo) {Employee.new({name: "Inigo Montoya", age: "25", salary: "120000"})}
+  let(:tyler) {Employee.new({name: "Tyler Durden", age: "25", salary: "85000"})}
 
   describe '#initialize' do 
     it 'exists and has a name' do 
       expect(budget).to be_a(Budget) 
       expect(budget.year).to eq(2010)
     end
+  end
+
+  describe 'departments/add_department' do 
+    expect(budget.departments).to eq([])
+
+    budget.add_department(customer_service)
+    budget.add_department(tech_support)
+
+    expect(budget.departments).to eq([customer_service, tech_support])
   end
 
 end
