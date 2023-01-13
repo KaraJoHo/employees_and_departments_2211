3bf0e29dbd41b5cefb4f0889949ef55a6751381f
KaraJH <karas.jones@yahoo.com>
2023-01-13 09:07:14 -0700

    feat: Employee class with attributes and passing test

---
+++ lib/employee.rb
@@ -0,0 +1,9 @@
+class Employee 
+  attr_reader :name, :age, :salary 
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @age = attributes[:age].to_i
+    @salary = attributes[:salary].delete('$').to_i
+  end
+end
