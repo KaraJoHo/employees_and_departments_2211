73c8c7a2315cbaf3e4dd9472144a95a921713c84
KaraJH <karas.jones@yahoo.com>
2023-01-13 11:22:42 -0700

    feat: Complete IT4. Department expense log hash method

--- lib/budget.rb
+++ lib/budget.rb
@@ -16,7 +16,7 @@ class Budget
     end
   end
 
-  def all_employees #helper, array of all employees
+  def all_employees 
     @departments.flat_map do |department| 
       department.employees
     end
@@ -29,5 +29,16 @@ class Budget
       employee_salary_hash[employee] = employee.salary
     end
     employee_salary_hash
+  end
+
+  def department_expense_log 
+    expense_log_hash = Hash.new({})
+
+    @departments.each do |department| 
+      expense_log_hash[department] = {:department_employee_expenses => department.employee_total_expenses, 
+                                      :department_total_expenses => department.expenses
+      }
+    end
+    expense_log_hash
   end
 end
