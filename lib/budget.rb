251fda8a4601b78e9cf247f4fd70fa3757618527
KaraJH <karas.jones@yahoo.com>
2023-01-13 10:03:27 -0700

    feat: Employee salaries method with passing test

--- lib/budget.rb
+++ lib/budget.rb
@@ -16,9 +16,18 @@ class Budget
     end
   end
 
-  def all_employees 
+  def all_employees #helper, array of all employees
     @departments.flat_map do |department| 
       department.employees
     end
+  end
+
+  def employee_salaries 
+    employee_salary_hash = {} 
+
+    all_employees.each do |employee| 
+      employee_salary_hash[employee] = employee.salary
+    end
+    employee_salary_hash
   end
 end
