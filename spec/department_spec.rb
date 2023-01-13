7c3b95213b48145fd75acc1d7d0d4873cfa29262
KaraJH <karas.jones@yahoo.com>
2023-01-13 09:16:20 -0700

    test: Hire method

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -4,11 +4,25 @@ require './lib/employee'
 RSpec.describe Department do 
   let(:customer_service) {Department.new("Customer Service")}
 
+  let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
+  let(:aaron) {Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})}
+
   describe '#initialize' do 
     it 'exists and has a name and list of employees' do 
       expect(customer_service).to be_a(Department)
       expect(customer_service.name).to eq("Customer Service")
       expect(customer_service.employees).to eq([])
+    end
+  end
+
+  describe '#hire' do 
+    it 'adds employees to the list of employees' do 
+      expect(customer_service.employees).to eq([]) 
+
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+      
+      expect(customer_service.employees).to eq([bobbi, aaron])
     end
   end
 end
