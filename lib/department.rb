005ec2ac2de655481c26b4324384f625489d36ff
KaraJH <karas.jones@yahoo.com>
2023-01-13 09:20:50 -0700

    feat: Expenses attribute and expense method with passing test

--- lib/department.rb
+++ lib/department.rb
@@ -1,12 +1,17 @@
 class Department 
-  attr_reader :name, :employees 
+  attr_reader :name, :employees, :expenses
 
   def initialize(name)
     @name = name 
     @employees = []
+    @expenses = 0
   end
 
   def hire(employee) #array of employee objects
     @employees << employee
+  end
+
+  def expense(amount)
+    @expenses += amount
   end
 end
